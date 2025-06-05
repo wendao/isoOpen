92e6c93ff7cc9a50f4917a7ace9b65cfa2a40145
wendao <pandawendao@gmail.com>
2025-06-05 10:46:23 +0800

    adding groundtruth

---
+++ reference_data/YJ7/stat.sh
@@ -0,0 +1,10 @@
+for d in L1H1 L1H2 L1H5 L1H10 L10H1 L5H1 L2H1
+do
+  cd $d/msf
+  awk -F'\t' 'NR>1{print $11}' QE_Plus_YJ_LZM_${d}_50per_20190725_F1_R1_ion_label_quant.tsv | gsl-histogram -8 8 80 > hist.txt
+  cd ../..
+
+  cd $d/pfd
+  grep IPM all_result.txt | awk -F'\t' '{if($16>0)print log($16)/log(2)}' | gsl-histogram -8 8 80 > hist.txt
+  cd ../..
+done
